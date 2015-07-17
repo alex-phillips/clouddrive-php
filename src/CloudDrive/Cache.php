2e5147652f1925b070346ae4cd5cf709b7df361f
alex-phillips <ahp118@gmail.com>
2015-07-16 20:15:59 -0400

    added documentation

--- src/CloudDrive/Cache.php
+++ src/CloudDrive/Cache.php
@@ -7,23 +7,89 @@
 
 namespace CloudDrive;
 
+/**
+ * Class that handles local storage of remote node information.
+ *
+ * @package CloudDrive
+ */
 interface Cache
 {
+    /**
+     * Delete all nodes from the cache.
+     *
+     * @return mixed
+     */
     public function deleteAllNodes();
 
+    /**
+     * Delete node with the given ID from the cache.
+     *
+     * @param string $id
+     *
+     * @return mixed
+     */
     public function deleteNodeById($id);
 
+    /**
+     * Find the node by the given ID in the cache.
+     *
+     * @param string $id
+     *
+     * @return mixed
+     */
     public function findNodeById($id);
 
+    /**
+     * Find the node by the given MD5 in the cache.
+     *
+     * @param string $md5
+     *
+     * @return mixed
+     */
     public function findNodeByMd5($md5);
 
+    /**
+     * Retrieve all node matching the given name in the cache.
+     *
+     * @param string $name
+     *
+     * @return mixed
+     */
     public function findNodesByName($name);
 
+    /**
+     * Retrieve all nodes who have the given node as their parent.
+     *
+     * @param Node $node
+     *
+     * @return mixed
+     */
     public function getNodeChildren(Node $node);
 
+    /**
+     * Retrieve the config for the account matched with the given email.
+     *
+     * @param string $email
+     *
+     * @return mixed
+     */
     public function loadAccountConfig($email);
 
+    /**
+     * Save the config for the provided account.
+     *
+     * @param Account $account
+     *
+     * @return mixed
+     */
     public function saveAccountConfig(Account $account);
 
+    /**
+     * Save the given node into the cache.
+     *
+     * @param Node $node
+     *
+     * @return mixed
+     */
     public function saveNode(Node $node);
 }
