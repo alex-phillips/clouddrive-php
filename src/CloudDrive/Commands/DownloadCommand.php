f3985f653276f33a2f4455982ac4c62fa29b49dd
alex-phillips <ahp118@gmail.com>
2015-08-07 18:36:19 -0400

    added support to download by node ID, also modified code to adhere to method changes in the node class

--- src/CloudDrive/Commands/DownloadCommand.php
+++ src/CloudDrive/Commands/DownloadCommand.php
@@ -7,6 +7,7 @@
 
 namespace CloudDrive\Commands;
 
+use CloudDrive\Node;
 use Symfony\Component\Console\Input\InputArgument;
 
 class DownloadCommand extends BaseCommand
@@ -16,22 +17,36 @@ class DownloadCommand extends BaseCommand
         $this->setName('download')
             ->setDescription('Download remote file to specified local path (currently only files are supported)')
             ->addArgument('remote_path', InputArgument::REQUIRED, 'The remote file path to download')
-            ->addArgument('local_path', InputArgument::OPTIONAL, 'The path to save the file');
+            ->addArgument('local_path', InputArgument::OPTIONAL, 'The path to save the file')
+            ->addOption('id', 'i', null, 'Designate the remote node by its ID instead of its remote path');
     }
 
     protected function main()
     {
         $this->init();
-        $this->clouddrive->getAccount()->authorize();
 
         $remotePath = $this->input->getArgument('remote_path');
-        $localPath = $this->input->getArgument('local_path');
+        $localPath = $this->input->getArgument('local_path') ?: '.';
 
-        $response = $this->clouddrive->downloadFile($remotePath, $localPath);
-        if ($response['success'] === false) {
-            $this->output->writeln("Failed to download file: " . json_encode($response['data']));
+        if ($this->input->getOption('id')) {
+            if (!($node = Node::loadById($remotePath))) {
+                throw new \Exception("No node exists with ID '$remotePath'.");
+            }
         } else {
-            $this->output->writeln("Successfully downloaded file to $localPath");
+            if (!($node = Node::loadByPath($remotePath))) {
+                throw new \Exception("No node exists at remote path '$remotePath'.");
+            }
+        }
+
+        if ($node->isFolder()) {
+            throw new \Exception("Folder downloads are not currently supported.");
+        }
+
+        $result = $node->download($localPath);
+        if ($result['success']) {
+            $this->output->writeln("Successfully downloaded file to '$localPath'.");
+        } else {
+            $this->output->writeln("Failed to download node to '$localPath': " . json_encode($result['data']));
         }
     }
 }
