7512ed298f0b21d8d4f1cd78b80c6fceae0b1a04
alex-phillips <ahp118@gmail.com>
2015-08-17 18:54:01 -0400

    added 'renew' command to manually renew authorization

---
+++ src/CloudDrive/Commands/RenewCommand.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author Alex Phillips <ahp118@gmail.com>
+ * Date: 8/17/15
+ * Time: 11:39 AM
+ */
+
+namespace CloudDrive\Commands;
+
+class RenewCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('renew')
+            ->setDescription('Renew authorization');
+    }
+
+    protected function main()
+    {
+        $this->init();
+
+        $result = $this->clouddrive->getAccount()->renewAuthorization();
+        if ($result['success']) {
+            $this->output->writeln("<info>Successfully renewed authorization.</info>");
+        } else {
+            $this->output->writeln("<error>Failed to renew authorization.</error>");
+        }
+    }
+}
